c84918771e3d8932bd99abbccc4aaa2066d0a9cf
root <none@none.com>
2020-08-05 21:55:52 +0000

    Day 3

--- _03_print_and_popups/_a_greeter.py
+++ _03_print_and_popups/_a_greeter.py
@@ -4,16 +4,19 @@ from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # Ask the user for their name and save it to a variable
+    name = simpledialog.askstring(title='question', prompt='What is your name')
     # name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
-    
+    age = simpledialog.askstring("first question", "Hello " + name + " how old are you?")
     # Show a message box with your message using the .showinfo() method
-    
+    messagebox.showinfo(title=None, message='Hello ' + name +  ", you are " + age)
+    messagebox.showerror(title=None, message="Err 404 Err")
     # Print your message to the console using the print() function
-    
+    window.print()
     # Show an error message using messagebox.showerror()
 
     # Run the window's .mainloop() method
+    window.mainloop()
--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,21 +1,27 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
-
+    userscore = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+    answer1 = simpledialog.askstring(title='question', prompt='how fast was the Cutty Sark(in knots)')
     #      // 3.  Use an if statement to check if their answer is correct
-
+    if answer1 == '17.5':
+        messagebox.showinfo(title=None, message='+1 point')
+        else
+        messagebox.showinfo(title=None, message='Wrong. Its 17.5 knots')
     #      // 4.  if the user's answer was correct, add one to their score 
- 
+    if answer1 == '17.5'
+        userscore = 1
+        else
+        userscore = -1
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
  

9c8e0d3cd7aeeb8d9378988603a6cecc03c011ac
root <none@none.com>
2020-08-07 21:53:50 +0000

    Cat.gif _a_terminator_cat.py

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -2,16 +2,16 @@ import turtle
 from PIL import Image
 
 class Eye():
-    def __init__(self, turtle=None, x=0, y=0, radius=30):
-        self.turtle = turtle
+    def __init__(self, myTurtle=None, x=0, y=0, radius=30):
+        self.myTurtle = myTurtle
         self.x = x
         self.y = y
         self.radius = radius
         
-        self.turtle.penup()
+
         
     def draw(self):
-        self.turtle.begin_fill()
+        self.myTurtle.begin_fill()
         self.myTurtle.goto(self.x, self.y)
         self.myTurtle.circle(radius=self.radius, steps=20)
         self.myTurtle.end_fill()
@@ -36,13 +36,16 @@ def keyPressed():
     print('You pressed the space key')
     
     # LASER BEAM.  This code will make your ellipse move down and to the right when you press 
-    # the space bar. Run the program to test it.
-
+    # the space bar. Run the program to test it
     # 10. Increment the x and y variables of the 2 eye variables by 5:
     #     leftEye.x += 5
-    
+    leftEye.x += 5
+    rightEye.x += 5
+    leftEye.y += 5
+    rightEye.y += 5
     # 11. Call the .draw() method for both eye variables.
-
+    leftEye.draw()
+    rightEye.draw()
 if __name__ == '__main__':
     window = turtle.Screen()
     
@@ -52,30 +55,33 @@ if __name__ == '__main__':
     #    c. Rename the image something short (e.g. cat.gif)
     #    d. Save the image to your computer's desktop
     #    e. Drag and drop the image into your _04_amazing_games python package
-    
-    # 2. Call the setBackground() function with your variable inside of the parenthesis
+
+    # 2. Call the set.backckground() function with your variable inside of the parenthesis
     #    for example, setBackground(bgImage)
-    
+    setBackground('Cat.gif')
     # 3. Make a new turtle
-    
+    dan = turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-    
+    dan.color('blue', 'blue')
+    dan.penup()
     # 5. Set the turtle width to 0 so no outlines are drawn
-    
+    dan.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-    
+    dan.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your processing window. 
     #    Variables for x and y have been created at the top of your sketch, 
     #    now you can set them equal to the values you just found. Watch for negative signs!
-    
+    #dan.goto(x=2, y=70)
     # 8. After you've found the x and y for the eyes create 2 eye variables and initialize them:
     #    leftEye = Eye(turtle=myTurtle, x=-34, y=11, radius=30)  
     #    rightEye = Eye(turtle=myTurtle, x=40, y=-5, radius=30)
-    
+    rightEye = Eye(myTurtle=dan, x=-54, y=54, radius=30)
+    leftEye = Eye(myTurtle=dan, x=3, y=48, radius=30)
     # 9. Call the .draw() method on both eye variables
-
+    leftEye.draw()
+    rightEye.draw()
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screenClicked)
     window.onkeypress(keyPressed, 'space')
